f3bc86a96021b6aa458ba8947579aec39dbb83e0
sakincse21 <saleheen.sakin@gmail.com>
2025-03-03 03:23:48 +0600

    test commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                             <img
-                                alt="Tailwind CSS Navbar component"
+                                alt="Navbar component"
                                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                         </div>
                     </div>
